19157cb8d8e823730f01b544d241fe7b098b1daf
Daniel Swietlik <56155234+daniel3108-pl@users.noreply.github.com>
2022-01-01 21:46:59 +0100

    Add files via upload

--- GaussEliminationASM/gaussElim.asm
+++ GaussEliminationASM/gaussElim.asm
@@ -1,33 +1,123 @@
+; Zmienne do zapisywania parametrow funkcji i licznikow petli
 .data
 j dq 0
 i dq 0
 k dq 0 
+m dq 0
+l dq 0
+pivotidx dq 0
 aii dq 0
 ajj dq 0
-sum dq 0
 rows dq 0
 cols dq 0
-
 matrix dq 0
-arr dq 0
+zero dq 0
 
-
+; Procedura sprowadzajaca macierz wejsciowa do postaci schodkowej
+; naglowek funkcji w c# : public static extern void gaussEliminationMASM(double[] matrix, int rows, int cols);
 .code
 gaussEliminationMASM PROC
+        mov		matrix, rcx
         mov     cols, r8
         mov     rows, rdx
 		mov		aii, 0
 		mov		ajj, 0
         mov     i, 0
         mov     j, 0
+		mov		zero, 0
 While1:
         mov     rax, rows
         cmp     i, rax
         jge     w1end
 
+; Ten Blok od if_aiieq0 do endifc jest odpowiedzialny za sprawdzenie czy element w macierzy[i][i] jest rowny 0
+; jesli jest nastepuje petla wyszukujaca od tylu macierzy czy jest jakis element w m-tym wierszu w i-tej kolumnie, ktory nie jest rowny 0
+; jesli nie jest rowny zero do zmiennej pivotidx jest zapisywane m i nastepuje potem zamiana wierszy miejscami
+if_aiieq0:
+		; sprawdzenie warunku matrix[i * cols + i] == 0
 		mov		rax, i
 		imul	rax, cols
 		add		rax, i
+		mov		rcx, matrix
+		movsd	xmm0, QWORD PTR [rcx + rax * 8]
+	    ucomisd xmm0, zero
+		jne		endifc
+thenif:
+; jest rowny wiec do pivotidx jest przypisywane i
+		mov		rax, i
+		mov		pivotidx, rax
+		; incializacja licznika m = rows - 1
+		mov		rax, rows
+		dec		rax
+		mov		m, rax
+while_m_gt_i:
+; petla while(m > i)
+		mov rax, i ; rax = i
+		cmp m, rax ; sprawdzenie m z rax
+		jle endwmlti 
+
+if_m_mne0:
+; warunek if ( matrix[m * cols + i] != 0 ) 
+		mov rax, m
+		imul rax, cols
+		add rax, i
+		mov rcx, matrix
+		movsd xmm0, QWORD PTR [rcx + rax *8]
+		ucomisd xmm0, zero ; porownanie rejestru xmm0  do 0
+		je endifmmi ; jesli rowne if sie nie wykonuje skok do endifmmi
+thenif2:
+		; przyjecie pivotidx = m i break z petli while_m_gt_i
+		mov rax, m
+		mov pivotidx, rax
+		jmp breakwmlti
+endifmmi:
+; jesli if sie nie wykonal nastepuje dekrementacja m i dalsza iteracja petli
+		mov rax, m
+		dec rax
+		mov m, rax
+		jmp while_m_gt_i
+endwmlti:
+; nieznaleziono zadnego wiersza gdzie element pocz nie jest 0, funkcja zwraca 0 => Macierz nie rozwiazywalna
+		mov rax, 0
+		ret
+breakwmlti:
+; licznik petli zamieniajacej wiersze 
+		mov l, 0
+whilerowsswap:
+; while ( l < l )
+		mov rax, cols
+		cmp l, rax
+		jge endifc
+
+		;; Zamiana wierszy w macierzy
+		; przygotowanie indeksu do wskaznika na matrix
+		mov rax, i
+		imul rax, cols
+		add rax, l
+		; r10 = wskaznik na matrix
+		mov r10, matrix
+		movsd xmm1, QWORD PTR [r10 + rax * 8] ; xmm1 = matrix[ i * cols + l ]
+		mov rdx, pivotidx
+		imul rdx, cols
+		add rdx, l
+		mov r9, matrix ; r1 = wskaznik na matrix
+		mov r8, matrix ; r8 = wskaznik na matrix
+		movsd xmm0, QWORD PTR [r9 + rdx * 8] ; xmm0 = matrix[ pivotidx * cols + l]
+		movsd QWORD PTR [r8 + rax * 8], xmm0 ; matrix[i * cols + l] = xmm0
+		mov rcx, matrix
+		movsd QWORD PTR [rcx + rdx * 8], xmm1 ; matrix[pivotidx * cols + l] = xmm1
+
+		mov rax, l
+		inc rax
+		mov l, rax ; l += 1 | incrementacja licznika l
+		jmp whilerowsswap
+
+endifc:
+; Reszta kodu zajmuje sie doprowadzeniem macierzy do postaci schodkowej
+		mov		rax, i
+		imul	rax, cols
+		add		rax, i
+		mov		rcx, matrix
 		movsd	xmm0, QWORD PTR [rcx + rax * 8]
 		movsd	aii, xmm0
 
@@ -56,12 +146,13 @@ While3:
 		imul	rax, cols
 		add		rax, k
 
-		mov		rdx, i
-		imul	rdx, cols
-		add		rdx, k
-
+		mov		rcx, i
+		imul	rcx, cols
+		add		rcx, k
+		mov		rdx, matrix
 		movsd	xmm0, ajj
-		mulsd	xmm0, QWORD PTR [rcx + rdx * 8]
+		mulsd	xmm0, QWORD PTR [rdx + rcx * 8]
+		mov		rcx, matrix
 		movsd	xmm1, QWORD PTR [rcx + rax * 8]
 		subsd	xmm1, xmm0
 		movaps	xmm0, xmm1
@@ -70,7 +161,8 @@ While3:
 		imul	rax, cols
 		adc		rax, k
 
-		movsd	QWORD PTR [rcx + rax * 8], xmm0
+		mov		rdx, matrix
+		movsd	QWORD PTR [rdx + rax * 8], xmm0
 		; ------ ;
 		mov		rax, k
 		inc		rax
@@ -88,6 +180,7 @@ w2end:
         mov     i, rax
         jmp		While1
 w1end:
-	ret
+	mov rax, 1
+	ret	
 gaussEliminationMASM ENDP
 end
--- GaussEliminationASM/gaussSubst.asm
+++ GaussEliminationASM/gaussSubst.asm
@@ -1,4 +1,4 @@
-
+; Zmienne przechowujace argumenty funkcji i indeksy do petli
 .data
 i dq 0
 j dq 0
@@ -7,9 +7,15 @@ cols dq 0
 matrix dq 0
 results dq 0
 
+; Procedura obliczajaca z macierzy schodkowej wartosci niewiadomych ukladu rownan
+; postac w c# : public static extern void gaussBackSubstMASM(double[] matrix, int rows, int cols, double[] results);
 .code
 gaussBackSubstMASM PROC
+; Zapisywanie do zmiennych wartosci parametrow procedury podanych z C#
+; Zeby moc korzystac z kazdego rejestru bez obawy przed utracenie danych
 start:
+		; rcx = wskaznik na tablice matrix | rdx = ilosc wierszy macierzy | r8 = ilosc kolumn macierzy
+		; r9 = wskaznik na tablice results
         mov		cols, r8
         mov     rows, rdx
 		mov		matrix, rcx
@@ -17,64 +23,92 @@ start:
         mov     rax, rows
         dec     rax
         mov     i, rax
+; Pierwsza petla idaca od tylu tablicy z wynikami
 while1:
+		; sparwdzenie warunku while'a
         cmp     i, -1
         jle     w1end
+		; Zapisanie do rax indeksu dla tablicy matrix z ktorego pobierany bedzie
+		; wartosc do zapisu na i-ta pozycje tablicy results => zapis wyrozu wolnego w rownaniu na i-tym wierszu
         mov     rax, i
         imul    rax, cols
         add     rax, rows
         mov		rcx ,i
+		; Zaladowanie do rdx wskaznika na tablice results oraz do r8 wskaznika na tablice macierzy
         mov     rdx, results
         mov     r8, matrix
+		; zapisanie do rejrestu xmm0 wartosci matrix[i * cols + rows]
         movsd   xmm0, QWORD PTR [r8+rax*8]
+		; kopiowanie zawartosci xmm0 do tablicy result na indexie i
         movsd   QWORD PTR [rdx+rcx*8], xmm0
+		
+		; przygotowanie licznika j = rows + 1 dla kolejnej petli  
         mov     rax, i
         inc     rax
         mov     j, rax
+; petla odejmujaca od wartosci result[i] obliczone poprzednie niewiadome * ich wspolczynniki
 while2:
+		; sprawdzenie warunku petli while(j < rows)
         mov     rax, rows
         cmp     j, rax
         jge     SHORT w2end
+		; przygotowanie indeksu i * cols + j dla macierzy do rejestru rcx
         mov		rax, i
         mov     rcx, i
         imul    rcx, cols
         add     rcx, j
         mov		rdx, j
+		; zapis do r8 wskaznika na matrix oraz do r9 wskaznika na resutls
         mov     r8, matrix
         mov     r9, results
+		; zapis do rejestru xmm0 wartosci matrix[ i * cols + j]
         movsd   xmm0, QWORD PTR [r8+rcx*8]
+		; pomnozenie wartosci xmm0 przez obliczona niewiadoma w poprzednim etapie pierwszego while'a
         mulsd   xmm0, QWORD PTR [r9+rdx*8]
         mov     rcx, results
+		; zapis do xmm1 wartosci naszego aktualnego wyrazu wolnego w rownaniu
         movsd   xmm1, QWORD PTR [rcx+rax*8]
+		; odjecie od xmm1 - xmm0 czyli od wyrazu wolnego odejmujemy j-ta niewiadoma * jej wspolczynnik 
         subsd   xmm1, xmm0
+		; zapis wartosci roznicy do rejestru xmm0
         movaps  xmm0, xmm1
+
         mov		rax, i
         mov     rcx, results
-        movsd   QWORD PTR [rcx+rax*8], xmm0
+        ; zapis w tablicy result na i-tej pozycji wartosci roznicy
+		movsd   QWORD PTR [rcx+rax*8], xmm0
         
+		; incrementacja licznika j 
 		mov     rax, j
         inc     rax
         mov     j, rax
+		; skok do poczatku petli
 		jmp		while2
+; cialo petli whlie 1 po zakonczeniu petli wewnetrznej
 w2end:
+		; przygotowanie indeksow matrix[ i * cols + i ] oraz i dla tablic wynikow
         mov		rax, i
         mov     rcx, i
         imul    rcx, cols
         add     rcx, i
         mov     rdx, results
         mov     r8, matrix
+		; zapis wyrazu wolnego results[i] do xmm0
         movsd   xmm0, QWORD PTR [rdx+rax*8]
+		; dzielenie w xmm0 wyrazu wolnego przez wartosc macierzy[ i* cols + i] czyli wspolczynnik dla szukanego x'a (niewiadomej)
         divsd   xmm0, QWORD PTR [r8+rcx*8]
         mov		rax, i
         mov     rcx, results
+		; zapis obliczonej wartosci niewiadomej na jej odpowiedniej pozycji i-tej
         movsd   QWORD PTR [rcx+rax*8], xmm0
 		
+		; decrementacja licznika 
 		mov     rax, i
         dec		rax
         mov     i, rax
         jmp     while1
+; koniec procedury powrot do miejsca jej wywolania
 w1end:
         ret     0
-
 gaussBackSubstMASM endp
 end
