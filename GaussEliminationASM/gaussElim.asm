cbc835b679c300cb03b3509bbbff79ebb4091b0a
Daniel Swietlik <56155234+daniel3108-pl@users.noreply.github.com>
2022-01-03 13:06:15 +0100

    Add files via upload

--- GaussEliminationASM/gaussElim.asm
+++ GaussEliminationASM/gaussElim.asm
@@ -6,33 +6,78 @@ k dq 0
 m dq 0
 l dq 0
 pivotidx dq 0
-aii dq 0
-ajj dq 0
 rows dq 0
 cols dq 0
 matrix dq 0
 zero dq 0
 
+
+ajj dq 0
+rowStartIndex dq 0 
+rowLen dq 0
+idx dq 0
 ; Procedura sprowadzajaca macierz wejsciowa do postaci schodkowej
 ; naglowek funkcji w c# : public static extern void gaussEliminationMASM(double[] matrix, int rows, int cols);
 .code
 gaussEliminationMASM PROC
-        mov		matrix, rcx
-        mov     cols, r8
-        mov     rows, rdx
-		mov		aii, 0
-		mov		ajj, 0
-        mov     i, 0
-        mov     j, 0
-		mov		zero, 0
-While1:
-        mov     rax, rows
-        cmp     i, rax
-        jge     w1end
+start:
+        mov     idx, r9
+        mov     rowLen, r8
+        mov     rowStartIndex, rdx
+        mov     matrix, rcx
 
-; Ten Blok od if_aiieq0 do endifc jest odpowiedzialny za sprawdzenie czy element w macierzy[i][i] jest rowny 0
+        mov     rax, idx
+        add     rax, rowStartIndex
+        mov     rcx, idx
+        imul    rcx, rowLen
+        add     rcx, idx
+
+        mov     rdx, matrix
+        mov     r8, matrix
+        movsd   xmm0, QWORD PTR [rdx+rax*8]
+        divsd   xmm0, QWORD PTR [r8+rcx*8]
+        movsd	ajj, xmm0
+        mov     i, 0
+while_i_lt_rowlen:
+        mov     rax, rowLen
+        cmp     i, rax
+        jge     endwhile
+        mov     rax, i
+        add     rax, rowStartIndex
+        mov     rcx, idx
+        imul    rcx, rowlen
+        add     rcx, i
+        mov     rdx, matrix
+        movsd   xmm0, QWORD PTR [rdx+rcx*8]
+        mulsd   xmm0, ajj
+        mov     rcx, matrix
+        movsd   xmm1, QWORD PTR [rcx+rax*8]
+        subsd   xmm1, xmm0
+        movaps  xmm0, xmm1
+        mov     rax, i
+        add    rax, rowStartIndex
+        mov     rcx, matrix
+        movsd   QWORD PTR [rcx+rax*8], xmm0
+
+	    mov     rax, i
+        inc     rax
+        mov     i, rax
+        jmp     while_i_lt_rowlen
+endwhile:
+        ret     0
+gaussEliminationMASM ENDP
+
+gaussPivotingMASM PROC
+	; Ten Blok od if_aiieq0 do endifc jest odpowiedzialny za sprawdzenie czy element w macierzy[i][i] jest rowny 0
 ; jesli jest nastepuje petla wyszukujaca od tylu macierzy czy jest jakis element w m-tym wierszu w i-tej kolumnie, ktory nie jest rowny 0
 ; jesli nie jest rowny zero do zmiennej pivotidx jest zapisywane m i nastepuje potem zamiana wierszy miejscami
+start:
+		mov		matrix, rcx
+        mov     cols, r8
+        mov     rows, rdx
+        mov     i, r9
+        mov     j, 0
+		mov		zero, 0
 if_aiieq0:
 		; sprawdzenie warunku matrix[i * cols + i] == 0
 		mov		rax, i
@@ -111,76 +156,9 @@ whilerowsswap:
 		inc		rax
 		mov		l, rax ; l += 1 | incrementacja licznika l
 		jmp		whilerowsswap
-
 endifc:
-; Reszta kodu zajmuje sie doprowadzeniem macierzy do postaci schodkowej
-		mov		rax, i
-		imul	rax, cols
-		add		rax, i
-		mov		rcx, matrix
-		movsd	xmm0, QWORD PTR [rcx + rax * 8]
-		movsd	aii, xmm0
+		mov rax, 1
+		ret 
+gaussPivotingMASM ENDP
 
-		mov		rax, i
-		inc		rax
-        mov     j, rax
-While2:
-		mov     rax, rows
-        cmp     j, rax
-        jge     w2end
-
-		mov		rax, j
-		imul	rax, cols
-		add		rax, i
-		movsd	xmm0, QWORD PTR [rcx + rax * 8]
-		divsd	xmm0, aii
-		movsd	ajj, xmm0
-
-		mov		k, 0
-While3:
-		mov		rax, cols
-		cmp		k, raX
-		jge		w3end
-
-		mov		rax, j
-		imul	rax, cols
-		add		rax, k
-
-		mov		rcx, i
-		imul	rcx, cols
-		add		rcx, k
-		mov		rdx, matrix
-		movsd	xmm0, ajj
-		mulsd	xmm0, QWORD PTR [rdx + rcx * 8]
-		mov		rcx, matrix
-		movsd	xmm1, QWORD PTR [rcx + rax * 8]
-		subsd	xmm1, xmm0
-		movaps	xmm0, xmm1
-
-		mov		rax, j
-		imul	rax, cols
-		adc		rax, k
-
-		mov		rdx, matrix
-		movsd	QWORD PTR [rdx + rax * 8], xmm0
-		; ------ ;
-		mov		rax, k
-		inc		rax
-		mov		k, rax
-		jmp		While3
-w3end:
-		mov		rax, j
-		inc		rax
-		mov		j, rax
-		jmp		While2
-w2end:
-		; ------- ;
-        mov     rax, i
-        inc     rax
-        mov     i, rax
-        jmp		While1
-w1end:
-	mov rax, 1
-	ret	
-gaussEliminationMASM ENDP
 end
